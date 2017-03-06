4ba4401e38fc06b3370bbdad881de3f04a006d30
Brian Bycynski <high5bri@hotmail.com>
2017-03-06 12:09:32 -0500

    just starting

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,18 @@ import React, { Component } from 'react'
 
 export default class App extends Component {
   render () {
+    const Atlantic = () => (
+      <div>
+        <h3>Atlantic Ocean</h3>
+        <p>Atlantic Ocean covers approcimately 1/5th of the surface of the planet</p>
+      </div>
+    )
+    const Pacific = () => (
+      <div>
+        <h3>Pacific Ocean</h3>
+        <p>Ferdinand Magellan, portugese explorer, named the ocean in 1521</p>
+      </div>
+    )
     return <div>
       <h2>Which Body of Water</h2>
       <ul>
