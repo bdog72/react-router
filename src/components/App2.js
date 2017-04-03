5cffd918d399b2987f7ede6aa4b16801a2a339a9
Brian Bycynski <high5bri@hotmail.com>
2017-04-03 07:04:05 -0400

    made a few changes

--- src/components/App2.js
+++ src/components/App2.js
@@ -1,6 +1,7 @@
-import React, { Component } from 'react'
+import React from 'react'
+import createHistory from 'history/createBrowserHistory'
 
-const Match = ({ pattern, component: Component }) => {
+const Match = ({ pattern, component: Component }, { location }) => {
   const pathname = window.location.pathname
   if (pathname.match(pattern)) {
     return (
@@ -11,31 +12,75 @@ const Match = ({ pattern, component: Component }) => {
   }
 }
 
-export default class App2 extends Component {
-  render () {
-    const Breakbeat = () => (
-      <div>
-        <h3>Breakbeat</h3>
-        <p>great to breakdance to</p>
-      </div>
-    )
-    const House = () => (
-      <div>
-        <h3>House</h3>
-        <p>great to dance to</p>
-      </div>
-    )
+Match.contextTypes = {
+  location: React.PropTypes.object
+}
 
-    return <div>
-      <h2>What kind of dance music you like</h2>
+const Link = ({ to, children }, { history }) => (
+  <a
+    onClick={(e) => {
+      e.preventDefault()
+      history.push(to)
+    }}
+    href={to}
+    >
+    {children}
+  </a>
+)
+
+Link.contextTypes = {
+  history: React.PropTypes.object
+}
+
+class Router extends React.Component {
+  static childContextTypes = {
+    history: React.PropTypes.object,
+    location: React.PropTypes.object
+  };
+  constructor (props) {
+    super(props)
+    this.history = createHistory()
+    this.history.listen(() => this.forceUpdate())
+  }
+  getChildContext () {
+    return {
+      history: this.history,
+      location: window.location
+    }
+  }
+
+  render () {
+    return this.props.children
+  }
+}
+
+const App2 = () => (
+
+  <Router>
+    <div>
+      <h2>What dance music you like</h2>
       <ul className='list2'>
-        <li><a href='/breakbeat'>Breakbeat</a></li>
-        <li><a href='/house'>House</a></li>
+        <li><Link to='/breakbeat'>Breakbeat</Link></li>
+        <li><Link to='/house'>House</Link></li>
         <li><a href='/progressive HOuse' />Progressive House</li>
       </ul>
       <hr />
       <Match pattern='/breakbeat' component={Breakbeat} />
       <Match pattern='/house' component={House} />
     </div>
-  }
-}
+  </Router>
+  )
+
+const Breakbeat = () => (
+  <div>
+    <h3>Breakbeat</h3>
+    <p>great to breakdance to</p>
+  </div>
+)
+const House = () => (
+  <div>
+    <h3>House</h3>
+    <p>great to dance to</p>
+  </div>
+)
+export default App2
